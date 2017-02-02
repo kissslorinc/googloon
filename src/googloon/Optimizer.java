7f0bdd1c6bda868bb48476f07d1fd82ac3613a4f
matesebi <matesebi@matesebi-ProBook>
2017-02-02 23:02:38 +0100

    optimized Optimizer

--- src/googloon/Optimizer.java
+++ src/googloon/Optimizer.java
@@ -1,14 +1,27 @@
 package googloon;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.TreeMap;
 
 public class Optimizer {
 	
 	public int run(HashMap<String, Integer> params){
-		new DFS(DataStore.loons.get(0).getVertex()).availableVertices();
-		HashSet tiles = null;
+		ArrayList<Vertex> vertices = new DFS(DataStore.loons.get(0).getVertex()).availableVertices();
+		HashSet<Tile> tileSet = new HashSet<>();
+		for(Vertex v : vertices){
+			tileSet.add(v.tileUnder());
+		}
+		TreeMap<Integer, Tile> tileTree = new TreeMap<>();
+		for(Tile tile : tileSet){
+			tileTree.put(tile.value(),tile);
+		}
+		for(Loon l : DataStore.loons){
+			//l.setDestinaton();
+		}
 		
 		return 0;
 	}
