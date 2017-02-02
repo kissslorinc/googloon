92049eb39bed3ded4061c45e2ec54b76eb7ee64d
Adam <Adam@192.168.137.240>
2017-02-02 21:46:27 +0100

    DataStore full

--- src/googloon/DataStore.java
+++ src/googloon/DataStore.java
@@ -18,16 +18,29 @@ public class DataStore {
 		DataStore.h= h;
 	}
 	
-	private static void initVertexes(){
-		
-	}
-	
 	public static int score(){
-		return 0;
+		int score = 0;
+		for (int i=0; i<map.length; i++){
+			for (int j = 0; j<map[i].length; j++){
+				score += map[i][j].isOnline();
+			}
+		}
+		return score;
 	}
 	
 	public static void cycle(){
-		
+		int i = 0;
+		while (i<loons.size()){
+			if (getVertexAt(loons.get(i).getPos(Coord.X), loons.get(i).getPos(Coord.Y), loons.get(i).getPos(Coord.Z)).nextVertex(loons.get(i).nextStep) == null){
+				loons.remove(i);
+			}
+			else {
+				loons.get(i).x = (getVertexAt(loons.get(i).getPos(Coord.X), loons.get(i).getPos(Coord.Y), loons.get(i).getPos(Coord.Z)).nextVertex(loons.get(i).nextStep)).x;
+				loons.get(i).y = (getVertexAt(loons.get(i).getPos(Coord.X), loons.get(i).getPos(Coord.Y), loons.get(i).getPos(Coord.Z)).nextVertex(loons.get(i).nextStep)).y;
+				loons.get(i).z = (getVertexAt(loons.get(i).getPos(Coord.X), loons.get(i).getPos(Coord.Y), loons.get(i).getPos(Coord.Z)).nextVertex(loons.get(i).nextStep)).z;
+				i++;
+			}
+		}
 	}
 	
 	public static Vertex getVertexAt(int x, int y, int z){
