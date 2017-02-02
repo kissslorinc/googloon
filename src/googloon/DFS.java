f83f21ce37cf859f48b579384b03aea08be5049f
matesebi <matesebi@matesebi-ProBook>
2017-02-02 22:13:45 +0100

    finished DFS

--- src/googloon/DFS.java
+++ src/googloon/DFS.java
@@ -1,12 +1,12 @@
 package googloon;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DFS {
 	HashMap<Vertex, Integer> depth;
 	HashMap<Vertex, Integer> finish;
 	HashMap<Vertex, Vertex> previous;
-	Vertex active;
 	int maxDepth;
 	int maxFinish;
 	Vertex start;
@@ -25,15 +25,36 @@ public class DFS {
 				previous.put(v, null);
 			}
 		}
-		active = start;
 		maxDepth = 1;
 		maxFinish = 0;
 		
-		//execution
+		execute(start);
+	}
+	
+	private void execute(Vertex active){
 		for(Vertex v : active.getNeighbours()){
-			if(depth.get(v) = )
+			if(depth.get(v) == Integer.MAX_VALUE){
+				maxDepth++;
+				depth.put(v, maxDepth);
+				previous.put(v, active);
+				execute(v);
+			}
+		}
+		maxFinish++;
+		finish.put(active, maxFinish);
+		if(previous.get(active) == null){
+			execute(previous.get(active));
 		}
 	}
 	
+	public ArrayList<Vertex> availableVertices(){
+		ArrayList<Vertex> ret = new ArrayList<Vertex>();
+		for(Vertex v : finish.keySet()){
+			if(finish.get(v) != Integer.MAX_VALUE){
+				ret.add(v);
+			}
+		}
+		return ret;
+	}
 	
 }
--- src/googloon/Optimizer.java
+++ src/googloon/Optimizer.java
@@ -2,11 +2,15 @@ package googloon;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Optimizer {
+	
 	public int run(HashMap<String, Integer> params){
-		DijkstraAlgorithm dij = new DijkstraAlgorithm((ArrayList<Vertex>) DataStore.vertices);
-		dij.execute(DataStore.loons.get(0).getVertex());
+		new DFS(DataStore.loons.get(0).getVertex()).availableVertices();
+		HashSet tiles = null;
+		
 		return 0;
 	}
+	
 }
