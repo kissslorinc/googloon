a260643d72fd21807957c89679cef691e12ba48b
Lorinc Kiss <kissslorinc@hotmail.com>
2017-02-02 22:41:35 +0100

    added parsing in main

--- src/googloon/Googloon.java
+++ src/googloon/Googloon.java
@@ -3,6 +3,7 @@ package googloon;
 public class Googloon {
 
 	public static void main(String[] args) {
+		Parser.read("in.txt");
 		Optimizer ultimateSuperOptimizer = new Optimizer();
 		ultimateSuperOptimizer.run(null);
 
