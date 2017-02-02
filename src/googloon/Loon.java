803f49bfdacd615e906e590656070b3079276206
Lorinc Kiss <kissslorinc@hotmail.com>
2017-02-02 20:08:41 +0100

    finished Loon members

--- src/googloon/Loon.java
+++ src/googloon/Loon.java
@@ -1,5 +1,36 @@
 package googloon;
 
 public class Loon {
-
+	int x,y,z,nextStep;
+	
+	Loon(int _x, int _y) {
+		x=_x;
+		y=_y;
+		z = 0;
+	}
+	
+	
+	public int getPos(Coord c) {
+		switch(c) {
+		case X:
+			return x;
+		case Y:
+			return y;
+		case Z: 
+			return z;
+		}
+		return -1;
+	}
+	
+	public void move(int i) {
+		nextStep = i;
+	}
+	
+	public boolean isOver(int x, int y) {
+		return this.x==x && this.y==y;}
+	
+	public Tile getTileUnder() {
+		return DataStore.map[x][y];
+		}
+	
 }
