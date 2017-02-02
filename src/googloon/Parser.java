d2bd5addc325d2c5f5d63cdb85e9a61730676ed2
Lorinc Kiss <kissslorinc@hotmail.com>
2017-02-02 19:35:03 +0100

    added coord values

--- src/googloon/Parser.java
+++ src/googloon/Parser.java
@@ -1,5 +1,32 @@
 package googloon;
 
-public class Parser {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class Parser {
+	static FileWriter fw=null;
+	
+	
+	static void read(String path) {
+		try {
+			FileReader fr=new FileReader(path);
+			BufferedReader br=new BufferedReader(fr);
+			
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	static void start() { }
+	static void write() { }
+	static void finish() {
+		fw=null;
+	}
 }
