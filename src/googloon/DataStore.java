6fa693dd8814f8458a636d271a02518c6ffe6f02
Adam <Adam@192.168.137.240>
2017-02-02 19:33:34 +0100

    added basic datastore members

--- src/googloon/DataStore.java
+++ src/googloon/DataStore.java
@@ -1,5 +1,31 @@
 package googloon;
 
-public class DataStore {
+import java.util.ArrayList;
+import java.util.List;
 
+public class DataStore {
+	
+	public static List<Vertex> vertexes = new ArrayList<Vertex>();
+	static int w, alt, h;
+	
+	public void initParameters(int w, int h, int alt){
+		DataStore.w= w;
+		DataStore.alt = alt;
+		DataStore.h= h;
+	}
+	
+	private static void initVertexList(){
+		for (int i=0; i<h; i++){
+			for (int j = 0; j<w; j++){
+				for (int k=0; k<h; k++){
+					vertexes.add(new Vertex());
+				}
+			}
+		}
+	}
+	
+	public static Vertex getVertexAt(int x, int y, int z){
+		return vertexes.get(z+x*alt + y*w*alt);
+	}
+	
 }
--- src/googloon/Vertex.java
+++ src/googloon/Vertex.java
@@ -1,5 +1,23 @@
 package googloon;
 
 public class Vertex {
-
+	
+	Vertex plus;
+	Vertex minus;
+	Vertex zero;
+	
+	public void initVertex(Vertex p, Vertex m, Vertex z){
+		plus = p;
+		minus = m;
+		zero = z;
+	}
+	
+	public Vertex nextVertex(int direction){
+		switch (direction){
+		case 0: return zero;
+		case 1: return plus;
+		case -1: return minus;
+		}
+		return null;
+	}
 }
