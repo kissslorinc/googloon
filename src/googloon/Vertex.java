1bc8e58f12495352b89cd66242ce2a285742cd8c
Adam <Adam@192.168.137.240>
2017-02-02 21:18:56 +0100

    Vertex full

--- src/googloon/Vertex.java
+++ src/googloon/Vertex.java
@@ -1,39 +1,55 @@
 package googloon;
 
 public class Vertex {
-		
+
 	int x;
 	int y;
 	int z;
-	
+
 	int windX;
 	int windY;
-	
-	public Vertex(int x, int y, int z, int windy, int windx){
+
+	public Vertex(int x, int y, int z, int windy, int windx) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		windX  = windx;
+		windX = windx;
 		windY = windy;
 	}
-	
-	public void initVertex(int x, int y){
-	
+
+	public Vertex nextVertex(int direction) {
+		switch (direction) {
+		case -1:
+			if (z == 1 || z == 0) {
+				return null;
+			}
+
+		case 1:
+			if (this.z == DataStore.a) {
+				return null;
+			}
+		}
+		if ((y + DataStore.getVertexAt(x, y, z + direction).windY) <= 0
+				|| (y + DataStore.getVertexAt(x, y, z + direction).windY) >= DataStore.a) {
+			return null;
+		}
+		return DataStore.getVertexAt(DataStore.getVertexAt(x, y, z + direction).windX + x,
+				DataStore.getVertexAt(x, y, z + direction).windY + y, z + direction);
 	}
-	
-	public Vertex nextVertex(int direction){
-		
-		
-		
-	/*else*/ return DataStore.getVertexAt(this.y + windY ,this.x + windX ,this.z + direction);
-	}
-	
-	public int getPos(Coord c){
-		switch (c){
-		case X: return this.x;
-		case Y: return this.y;
-		case Z: return this.z;
+
+	public int getPos(Coord c) {
+		switch (c) {
+		case X:
+			return this.x;
+		case Y:
+			return this.y;
+		case Z:
+			return this.z;
 		}
 		return -1;
+	}
+	
+	public Tile tileUnder(){
+		return DataStore.map[x][y]; 
 	}
 }
